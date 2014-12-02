0abcf1ca79e1cf060f8fe93de2079633e7a00ce3
kyle@condoresorts.com <paladin9x>
2014-12-01 17:09:52 -0700

    test

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
 
-            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,5 +25,12 @@ namespace WindowsFormsApplication1
 
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
+
 }
+
